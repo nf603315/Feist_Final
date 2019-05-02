c1abfde2f832596126754251e55594be7cd5cc4b
nf603315 <47158811+nf603315@users.noreply.github.com>
2019-05-02 13:48:29 -0400

    Feist Final

---
+++ Feist_3401_Final/Assets/Scripts/AgentMotor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class AgentMotor : MonoBehaviour
+{
+    public Camera MainCamera;
+    public GameObject Player;
+    public float Speed = 5;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = MainCamera.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            float step = Speed * Time.deltaTime;
+            transform.position = (ray.direction);
+        }
+
+    }
+}
+
